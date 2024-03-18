59cae04f5048d8e77760230bb4d9868aad5b0f73
JoelEspinal <joel_espinal@claro.com.do>
2024-03-18 12:04:18 -0400

    Add table view content

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var itemArray = ["A", "B", "C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // MARK - Tableview DatasourceMethodd
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView,  cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
     }
 }
-
