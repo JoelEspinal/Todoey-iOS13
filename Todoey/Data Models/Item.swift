ed7b08e099aba98ce18c1205e33d9e66b5a888bf
JoelEspinal <joel_espinal@claro.com.do>
2024-03-20 11:45:36 -0400

    Add Item model

--- Todoey/Data Models/Item.swift
+++ Todoey/Data Models/Item.swift
@@ -2,8 +2,14 @@
 //  Item.swift
 //  Todoey
 //
-//  Created by Joel Espinal (ClaroDom) on 20/3/24.
+//  Created by Joel Espinal on 20/3/24.
 //  Copyright © 2024 App Brewery. All rights reserved.
 //
 
 import Foundation
+
+class Item {
+    
+    let title: String = ""
+    let donde: Bool = false
+}
