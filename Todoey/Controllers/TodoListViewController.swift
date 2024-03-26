7f83e57b4c4b0ffae2b031a7ee424e341e3cc471
JoelEspinal <joel_espinal@claro.com.do>
2024-03-26 16:08:28 -0400

    fetch ascending items request

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -117,5 +117,15 @@ extension TodoListViewController: UISearchBarDelegate {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         let predicate = NSPredicate(format: "title Contains [cd] %@", searchBar.text!)
         request.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
     }
 }
