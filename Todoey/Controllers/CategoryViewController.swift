105c167bae5791da839bf77fb6e4155be1ce566f
JoelEspinal <joel_espinal@claro.com.do>
2024-03-27 16:42:45 -0400

    with errors

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,23 +7,87 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    
+    var itemArray: [Category] = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     
     // MARK: - TableView Datasource Methods
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView,  cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryItemCell", for: indexPath)
+        
+        let category = itemArray[indexPath.row]
+        cell.textLabel?.text = category.name
+        
+      
+        
+        return cell
+    }
+    
     
     // MARK: - Data Manipulation Methods
     
+    func saveItems() {
+//        let encoder = PropertyListEncoder()
+        
+        do {
+            try self.context.save()
+        } catch {
+            print("Error saving context. \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
+            tableView.reloadData()
+        } catch {
+            printContent("Error fetching data from context \(error)")
+        }
+    }
+    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let categoryName = textField.text!
+            if !categoryName.isEmpty {
+                let newItem = Category(context: self.context)
+                newItem.name = categoryName
+                self.itemArray.append(newItem)
+                self.saveItems()
+            }
+        }
+    
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
     
