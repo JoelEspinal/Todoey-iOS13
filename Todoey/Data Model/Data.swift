10ea0508cae566734e030273b3995b483624cc90
JoelEspinal <joel_espinal@claro.com.do>
2024-04-05 10:24:00 -0400

    Save realm object

---
+++ Todoey/Data Model/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Joel Espinal (ClaroDom) on 5/4/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+}
