eac682a0907b6f6e1a2cc39320420c6b9628d7ae
JoelEspinal <joel_espinal@claro.com.do>
2024-04-09 08:36:21 -0400

    Change arrays to List

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -14,15 +14,15 @@ class CategoryViewController: UITableViewController {
 
     let realm = try! Realm()
     
-    var items = List<Category>()
+    var categories = List<Category>()
+    var items = List<Item>()
     
-    var itemArray: [Category] = [Category]()
+//    var itemArray: [Category] = [Category]()
 //    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -31,13 +31,13 @@ class CategoryViewController: UITableViewController {
     // MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView,  cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let categoryCell = tableView.dequeueReusableCell(withIdentifier: "CategoryItemCell", for: indexPath)
         
-        let category = itemArray[indexPath.row]
+        let category = categories[indexPath.row]
         categoryCell.textLabel?.text = category.name
         
         return categoryCell
@@ -47,14 +47,10 @@ class CategoryViewController: UITableViewController {
     // MARK: - Data Manipulation Methods
     
     func save(category: Category) {
-//        let encoder = PropertyListEncoder()
-        
         do {
             try realm.write {
                 realm.add(category)
             }
-            
-        
         } catch {
             print("Error saving context. \(error)")
         }
@@ -65,15 +61,13 @@ class CategoryViewController: UITableViewController {
     func loadCategories(with request: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: String(describing: Category.self))) {
     
         guard let result = realm.objects(Category.self).first else {return}
-        
-//            items = result.items
-            tableView.reloadData()
+        items = result.items
+        tableView.reloadData()
     }
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
 
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
@@ -81,8 +75,8 @@ class CategoryViewController: UITableViewController {
             if !categoryName.isEmpty {
                 let category = Category()
                 category.name = categoryName
-                self.itemArray.append(category)
-                
+                self.categories.append(category)
+
                 self.save(category: category)
             }
         }
@@ -101,7 +95,7 @@ class CategoryViewController: UITableViewController {
     
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let categoryItem = itemArray[indexPath.row]
+//        let categoryItem = categories[indexPath.row]
         
         performSegue(withIdentifier: "goToItems", sender: self)
     }
@@ -110,7 +104,7 @@ class CategoryViewController: UITableViewController {
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = itemArray[indexPath.row]
+            destinationVC.selectedCategory = categories[indexPath.row]
         }
     }
 }
